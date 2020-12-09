97973020fa896d11cc86e2d0901561c6e844307c
unknown <bettydobre@gmail.com>
2020-12-09 17:51:33 +0200

    add new product and started home page

--- app/src/main/java/com/shop/AdminCategoryActivity.java
+++ app/src/main/java/com/shop/AdminCategoryActivity.java
@@ -30,6 +30,15 @@ public class AdminCategoryActivity extends AppCompatActivity {
             }
         });
 
+        decorations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "decorations");
+                startActivity(intent);
+            }
+        });
+
         lipsticks.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/shop/MainActivity.java
+++ app/src/main/java/com/shop/MainActivity.java
@@ -1,6 +1,7 @@
 package com.shop;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -11,6 +12,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
     private Button joinNowButton, loginButton;
     private ProgressDialog loadingBar;
 
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    private int RC_SIGN_IN = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
         joinNowButton = findViewById(R.id.main_join_now_btn);
         loginButton = findViewById(R.id.main_login_btn);
         loadingBar = new ProgressDialog(this);
+
+        signInButton = findViewById(R.id.main_google_btn);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInWithGoogle();
+            }
+        });
 
         Paper.init(this);
 
@@ -73,6 +106,97 @@ public class MainActivity extends AppCompatActivity {
         return string.replace(".", ",");
     }
 
+
+    //implemented SIGN IN option with Google
+    private void signInWithGoogle() {
+        Intent signInClient = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInClient, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
+            FirebaseGoogleAuth(acc);
+        }
+        catch (ApiException e){
+            Toast.makeText(MainActivity.this, "Sign In failed", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(null);
+        }
+    }
+
+    private void FirebaseGoogleAuth(GoogleSignInAccount acct) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    loadingBar.setTitle("Login with Google");
+                    loadingBar.setMessage("Please wait, we are checking the credentials.");
+                    loadingBar.setCanceledOnTouchOutside(false);
+                    loadingBar.show();
+
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+                    String email = account.getEmail();
+                    String name = account.getDisplayName();
+                    String password = "";
+
+                    final DatabaseReference RootRef;
+                    RootRef = FirebaseDatabase.getInstance().getReference();
+
+                    RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.child("Users").child(EncodeString(email)).exists()) {
+                                Toast.makeText(MainActivity.this, "There is already an account with this email", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                mGoogleSignInClient.signOut();
+                                signInWithGoogle();
+                            }
+                            else if (!dataSnapshot.child("Users").child(EncodeString(email)).exists()){
+                                Toast.makeText(MainActivity.this, "Signed In successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+                            else if (dataSnapshot.child("Admins").child(EncodeString(email)).exists()){
+                                Toast.makeText(MainActivity.this, "There is already an admin account with this email", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                mGoogleSignInClient.signOut();
+                                signInWithGoogle();
+                            }
+                            else if (!dataSnapshot.child("Admins").child(EncodeString(email)).exists()){
+                                    Toast.makeText(MainActivity.this, "Signed In successfully", Toast.LENGTH_SHORT).show();
+                                    loadingBar.dismiss();
+                                    Intent intent = new Intent(MainActivity.this, AdminCategoryActivity.class);
+                                    startActivity(intent);
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    //login from Remember me option
     private void AllowAccess(final String email, final String password) {
         final DatabaseReference RootRef;
         RootRef = FirebaseDatabase.getInstance().getReference();
@@ -105,7 +229,6 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
     }
