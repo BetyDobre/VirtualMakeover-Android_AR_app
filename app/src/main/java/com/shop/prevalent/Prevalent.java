9f244572ee2128f36c1b8a9d64d094b53b416d7a
unknown <bettydobre@gmail.com>
2020-12-10 13:49:19 +0200

    home activity and settings

--- app/src/main/java/com/shop/prevalent/Prevalent.java
+++ app/src/main/java/com/shop/prevalent/Prevalent.java
@@ -3,7 +3,7 @@ package com.shop.prevalent;
 import com.shop.models.Users;
 
 public class Prevalent {
-    private static Users currentOnlineUser;
+    public static Users currentOnlineUser;
 
     public static final String UserEmailKey = "useremail";
     public static final String UserPasswordKey = "userpassword";
